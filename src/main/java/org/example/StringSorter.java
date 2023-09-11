0835b1bf981a6c36d784f44d115280a6c660b63c
ospodaryk <ospodaryk11@gmail.com>
2023-09-11 20:56:47 +0300

    Optimised my method and added javadoc

--- src/main/java/org/example/StringSorter.java
+++ src/main/java/org/example/StringSorter.java
@@ -1,68 +1,73 @@
 package org.example;
 
-import java.util.*;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The StringSorter class defines the contract for sorting strings based on
+ * alphabetical order while skipping words that start with a specific prefix.
+ * <p>
+ * Example:
+ * Input: ["apple", "banana", "grape", "avocado", "cherry"], Exception prefix: "a"
+ * Output: ["banana", "cherry", "grape", "avocado", "apple"]
+ * <p>
+ * Here, "banana", "cherry", and "grape" are sorted in alphabetical order
+ * whereas "avocado" and "apple" (that start with 'a') are sorted in reverse alphabetical order
+ * at the end of the list.
+ */
 public class StringSorter {
-
+    /**
+     * Sorts a list of strings based on the provided exception character.
+     *
+     * @param unsortedStrings List of unsorted strings.
+     * @param exceptionChar   The specific prefix.
+     * @return List of sorted strings.
+     */
     public List<String> sortStrings(List<String> unsortedStrings, String exceptionChar) {
-
-        List<String> filteredAndSortedStrings = filterAndSortStrings(unsortedStrings);
-
-        if (isListEmptyOrNull(unsortedStrings) || exceptionChar == null) {
-            return filteredAndSortedStrings;
-        }
-        if (exceptionChar.isEmpty()) {
-            return sortWhenCharIsEmpty(filteredAndSortedStrings);
-        }
-        return sortConsideringExceptionChar(filteredAndSortedStrings, exceptionChar);
-    }
-
-    private boolean isListEmptyOrNull(List<String> unsortedStrings) {
-        return unsortedStrings == null || unsortedStrings.isEmpty();
-    }
-
-    private List<String> filterAndSortStrings(List<String> unsortedStrings) {
-        return unsortedStrings.stream()
+        List<String> filteredAndSortedStrings = unsortedStrings.stream()
                 .filter(Objects::nonNull)
                 .sorted()
                 .collect(Collectors.toList());
+
+        if ((exceptionChar == null) || (isListEmptyOrNull(unsortedStrings))) {
+            return filteredAndSortedStrings;
+        }
+
+        return sortByPrefix(filteredAndSortedStrings, exceptionChar);
     }
 
-    private List<String> sortWhenCharIsEmpty(List<String> sortedStrings) {
-        List<String> stringsWithExceptionPrefix = new ArrayList<>();
-        List<String> otherStrings = new ArrayList<>();
+    /**
+     * Checks if the list is empty or null.
+     *
+     * @param list The list to be checked.
+     * @return True if the list is null or empty, false otherwise.
+     */
+    private boolean isListEmptyOrNull(List<String> list) {
+        return list == null || list.isEmpty();
+    }
 
+    /**
+     * Handles the sorting logic for strings based on the exception character.
+     *
+     * @param sortedStrings The list of already sorted strings.
+     * @param exceptionChar The exception character.
+     * @return The sorted list of strings considering the exception character.
+     */
+    private List<String> sortByPrefix(List<String> sortedStrings, String exceptionChar) {
+        List<String> exceptionPrefixStrings = new ArrayList<>();
+        List<String> otherStrings = new ArrayList<>();
         for (String string : sortedStrings) {
-            if (string.isEmpty()) {
-                stringsWithExceptionPrefix.add(string);
+            if (exceptionChar.isEmpty() ? string.isEmpty() : string.startsWith(exceptionChar)) {
+                exceptionPrefixStrings.add(string);
             } else {
                 otherStrings.add(string);
             }
         }
-        stringsWithExceptionPrefix.sort(Comparator.reverseOrder());
-        otherStrings.addAll(stringsWithExceptionPrefix);
-
-        return otherStrings;
-    }
-
-    private List<String> sortConsideringExceptionChar(List<String> sortedStrings, String exceptionChar) {
-        List<String> stringsWithExceptionPrefix = new ArrayList<>();
-        List<String> otherStrings = new ArrayList<>();
-        for (String string : sortedStrings) {
-            if (string.startsWith(exceptionChar)) {
-                stringsWithExceptionPrefix.add(string);
-            } else {
-                otherStrings.add(string);
-            }
-        }
-        stringsWithExceptionPrefix.sort(Comparator.reverseOrder());
-        otherStrings.addAll(stringsWithExceptionPrefix);
+        exceptionPrefixStrings.sort(Comparator.reverseOrder());
+        otherStrings.addAll(exceptionPrefixStrings);
         return otherStrings;
     }
 }
